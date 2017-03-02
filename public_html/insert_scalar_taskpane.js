a38d639a6f81359474fd1782e110891b6056e938
Giovanni <lomagno.gl@virgilio.it>
2017-03-02 18:02:54 +0100

    Get scalar prototype completed

--- public_html/insert_scalar_taskpane.js
+++ public_html/insert_scalar_taskpane.js
@@ -3,22 +3,50 @@
 'use strict';
 
 (function () {   
-    var scalarNameTextEdit, decimalsTextEdit;
+    var scalarNameTextEdit,
+        decimalsTextEdit,
+        decimalsErrorMsg,
+        scalarNameErrorMsg,
+        errorMsg,
+        errorMsgText,
+        insertScalarButton,
+        stataNameRx = new RegExp(/^[a-zA-Z_][a-zA-Z_0-9]{0,31}$/),
+        isScalarNameValid = false,
+        isDecimalsValid = true;
     
-    Office.initialize = function (reason) {
-        $(document).ready(function () {
+    Office.initialize = function (/* reason */) {
+        $(document).ready(function () {                        
+            // Scalar name text edit
             scalarNameTextEdit = $('#scalarNameTextEdit');
-            decimalsTextEdit = $('#decimalsTextEdit');
+            scalarNameTextEdit.on('input', onScalarNameTextEditChanged);
+            scalarNameErrorMsg = $('#scalarNameErrorMsg');
             
-            $('#insertScalarButton').click(retrieveScalar);
+            // Decimals text edit
+            decimalsTextEdit = $('#decimalsTextEdit');
+            decimalsTextEdit.on('input', onDecimalsTextEditChanged);
+            decimalsErrorMsg = $('#decimalsErrorMsg');
+            
+            // Error message
+            errorMsg = $('#error-msg');            
+            $('#error-msg-close-link').click(function(event) {
+                event.preventDefault();
+                errorMsg.hide();
+            });
+            errorMsgText = $('#error-msg-text');
+            
+            // Insert scalar button
+            insertScalarButton = $('#insertScalarButton');
+            $('#insertScalarButton').click(onInsertScalarButtonClicked);
+            
+            // Stata name regular expression
+            stataNameRx = new RegExp(/^[a-zA-Z_][a-zA-Z_0-9]{0,31}$/);            
         });
-    };       
+    };   
     
-    
-    
-    function retrieveScalar() {
-        var scalarName = scalarNameTextEdit.val();
-        var decimals = decimalsTextEdit.val();
+    function onInsertScalarButtonClicked() {
+        errorMsg.hide();
+        var scalarName = scalarNameTextEdit.val().trim();
+        var decimals = decimalsTextEdit.val().trim();
         
         var request = {
             job: [
@@ -34,12 +62,31 @@
             data: swire.encode(request),
             method: "POST",
             success: function (data) {
+                // Decode response
                 var response = swire.decode(data);
+                
+                // Check errors
+                if (response.status !== 'ok') {
+                    showErrorMsg('SWire returned an error');
+                    return;
+                }                
+                if (response.output[0].status !== 'ok') {
+                    showErrorMsg('SWire returned an error');
+                    return;                    
+                }                
+                if (response.output[0].output === null) {
+                    showErrorMsg('Not existing scalar');
+                    return;
+                }
+                
+                // Scalar value
                 var scalarValue = response.output[0].output;
+                
+                // Insert scalar value in Word
                 insertNumber(scalarValue, decimals);
             },
-            error: function () {
-                console.log('network error');
+            error: function (/* jqXHR, textStatus, errorThrown */) {
+                showErrorMsg('Cannot communicate with Stata');
             }
         });        
     }
@@ -52,5 +99,68 @@
                 console.log(error.name + ": " + error.message);                 
             }
         });        
+    }
+    
+    function onScalarNameTextEditChanged() {
+        errorMsg.hide();
+        
+        // Validate scalar name
+        var text = $(this).val().trim();
+        if (text === '') {
+            isScalarNameValid = false;
+            scalarNameErrorMsg.text('A Stata scalar name is required');
+            scalarNameErrorMsg.show();            
+        }
+        else if (!stataNameRx.test(text)) {
+            isScalarNameValid = false;
+            scalarNameErrorMsg.text('Not valid Stata scalar name');
+            scalarNameErrorMsg.show();
+        }
+        else {
+            isScalarNameValid= true;
+            scalarNameErrorMsg.hide();
+        }
+        
+        updateInsertScalarButtonStatus();
+    }
+    
+    function onDecimalsTextEditChanged() {
+        errorMsg.hide();
+        
+        // Validate decimals
+        var text = $(this).val().trim();
+        if (text === '') {
+            isDecimalsValid = false;
+            decimalsErrorMsg.text('Decimals must be set');
+            decimalsErrorMsg.show();
+        }
+        else if (!($.isNumeric(text) && Number.isInteger(+text))) {
+            isDecimalsValid = false;
+            decimalsErrorMsg.text('An integer number must be entered');
+            decimalsErrorMsg.show();
+        }
+        else if (+text < 0 || +text > 20) {
+            isDecimalsValid = false;
+            decimalsErrorMsg.text('A integer value between 0 and 20 is required');
+            decimalsErrorMsg.show();
+        }
+        else {
+            isDecimalsValid = true;
+            decimalsErrorMsg.hide();
+        }
+        
+        updateInsertScalarButtonStatus();
+    }
+
+    function updateInsertScalarButtonStatus() {
+        if (isScalarNameValid && isDecimalsValid)
+            insertScalarButton.prop('disabled', false);
+        else
+            insertScalarButton.prop('disabled', true);
+    }
+    
+    function showErrorMsg(msg) {
+        errorMsgText.text(msg);
+        errorMsg.show();
     }
 })();
