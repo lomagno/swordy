9e97f1e78ac111f44939062ed26f778cfc412ca5
Giovanni <lomagno.gl@virgilio.it>
2017-05-12 00:17:08 +0200

    Improved documentation and managed web spiders

--- webapp/js/documentation_taskpane.js
+++ webapp/js/documentation_taskpane.js
@@ -1,27 +1,43 @@
 /* global Office, fabric */
 
-"use strict";
+// "use strict";
 
 (function () {    
     var
         m_topicDropdownElement,
+        m_topicDropdownItemsMap = new Map,
         m_topicContainers;
         
     // The initialize function is run each time the page is loaded.
     Office.initialize = function () {
         $(document).ready(function () {
+            var initialTopic = 'swire';
+            
             // Topic dropdown
             m_topicDropdownElement = $('#topicDropdown');
-            new fabric['Dropdown'](m_topicDropdownElement[0]);
-            m_topicDropdownElement.find('.ms-Dropdown-select').change(onTopicChanged); 
+            var fabricTopicDropdown = new fabric['Dropdown'](m_topicDropdownElement[0]);
+            for (var i=1; i<fabricTopicDropdown._dropdownItems.length; ++i) {
+                var key = fabricTopicDropdown._dropdownItems[i].oldOption.value;
+                var value = fabricTopicDropdown._dropdownItems[i].newItem;
+                m_topicDropdownItemsMap.set(key, value);
+            }
+            setTopicDropdownItem(initialTopic);
+            m_topicDropdownElement.find('.ms-Dropdown-select').change(onTopicChanged);             
             
             // Topic containers
             m_topicContainers = $('.topic-container');
             
+            // Topic links
+            $('.topic-link').click(onTopicLinkClicked);
+            
             // Show initial topic
-            showTopic('about');
+            showTopic(initialTopic);
         });
     };
+    
+    function setTopicDropdownItem(topic) {
+        $(m_topicDropdownItemsMap.get(topic)).click();
+    }
     
     function showTopic(topic) {
         m_topicContainers.hide();
@@ -31,5 +47,11 @@
     function onTopicChanged() {
         var topic = m_topicDropdownElement.find('option:checked').val();        
         showTopic(topic);        
+    }
+    
+    function onTopicLinkClicked() {
+        var topicLink = $(this);
+        var topic = topicLink.data('topic-target');
+        setTopicDropdownItem(topic);
     }
 })();
